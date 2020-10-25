ac8e8aaac425d2d34799f1951616dbf6bffef49d
JRyGithub <nzryland@gmail.com>
2020-10-25 14:18:54 +1300

    front-end

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -103,7 +103,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -136,24 +137,31 @@ class App extends Component {
       .catch(err => console.log(err));
   }
   onRouteChange =(route) =>{
+    if(route === 'signout'){
+      this.setState({isSignedIn: false})
+    }
+      else if(route ==='home'){
+        this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
   render(){
+    const {isSignedIn, imageUrl, route, box} = this.state;
       return (
         <div className="App">
           <Particles className='particles' params={particlesOptions}/>
-          <Navigation onRouteChange = {this.onRouteChange}/>
+          <Navigation isSignedIn={isSignedIn} onRouteChange = {this.onRouteChange}/>
           { 
-            this.state.route === 'home' ?
+            route === 'home' ?
             <div> 
                 <Logo/> 
                 <Rank/>
                 <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+                <FaceRecognition box={box} imageUrl={imageUrl} />
               </div>
               : 
               (
-                this.state.route === 'signin' ?   
+                route === 'signin' ?   
                 <SignIn onRouteChange= {this.onRouteChange}/> 
                 :
                 <Register onRouteChange = {this.onRouteChange} />
